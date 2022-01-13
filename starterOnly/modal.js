535aa73006bdd7cdd54eac4bc837f668ebd849a0
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-01-13 23:05:28 +0100

    Ajout de la fonctionnalité de fermeture de la modale, correction de la double scroll bar

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const body = document.querySelector('body');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +20,16 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  body.style.overflow = "hidden";
 }
+
+// close modal form
+const closeModal = () => {
+  modalbg.style.display = "none";
+  body.style.overflow = "visible";
+}
+
+closeBtn.addEventListener('click', closeModal);
+
 
 
