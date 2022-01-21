4a7caeb582342a64c5f8281fec197b44e06ae398
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-01-21 09:33:53 +0100

    Avancement validation des inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const body = document.querySelector('body');
+const form = document.querySelector('form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,7 +30,76 @@ const closeModal = () => {
   body.style.overflow = "visible";
 }
 
-closeBtn.addEventListener('click', closeModal);
 
+
+
+// formulaire
+const handleForm = () => {
+  
+  const inputs = document.querySelectorAll('.formulaire');
+
+  for (const input of inputs) {
+    
+    const type = input.type;
+    
+    switch (type) {
+
+      case 'text': 
+        if (input.value == null || input.value == '') {
+          const error = document.createElement('p');
+          error.innerHTML = 'Veuillez saisir un nom';
+          error.id = 'error';
+          input.insertAdjacentElement('afterend', error);
+        } else {
+          console.log('fail');
+        }
+      break;
+
+      case 'email':
+          if (input.value == null || input.value == '') {
+            const error = document.createElement('p');
+            error.innerHTML = 'Veuillez saisir une adresse mail valide';
+            error.id = 'error';
+            input.insertAdjacentElement('afterend', error);
+          } else {
+            console.log('fail_email');
+          }
+
+      case 'date':
+          if (input.value == null || input.value == '') {
+            const error = document.createElement('p');
+            error.innerHTML = 'Veuillez saisir une date';
+            error.id = 'error';
+            input.insertAdjacentElement('afterend', error);
+          } else {
+            console.log('fail_date');
+          }
+
+      case 'radio':
+          let test = false;
+
+          console.log('test_radio' + inputs);
+          for (const element of inputs) {
+            if(element.type == 'radio' && element.checked) {
+              test = true;
+            }
+          }
+
+          if (test != true) {
+            const error = document.createElement('p');
+            error.innerHTML = 'Veuillez saisir une date';
+            error.classList.add('error_radio');
+            error.id = 'error';
+          } else {
+            console.log('fail_radio');
+          }
+
+
+    }
+  }
+}
+
+
+closeBtn.addEventListener('click', handleForm);
 
 
