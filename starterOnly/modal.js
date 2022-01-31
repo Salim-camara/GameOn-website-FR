4fc369381ca554bb637c5cada0332a57c43c3f11
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-01-31 22:30:54 +0100

    Avancement de la fonction des inputs -- ajout du style princpalement

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,54 +34,61 @@ const closeModal = () => {
 
 
 // formulaire
-const handleForm = () => {
+const handleForm = async () => {
   
   const inputs = document.querySelectorAll('.formulaire');
+  const errors = document.querySelectorAll('.error');
+
+  for(const element of errors) {
+    element.remove();
+  }
+  for(const element of inputs) {
+    element.style.border = 'none';
+  }
 
   for (const input of inputs) {
     
     const type = input.type;
+    const error = (messageErreur) => {
+      const error = document.createElement('p');
+      error.innerHTML = `${messageErreur}`;
+      error.style.color = 'red';
+      error.style.fontSize = '15px';
+      input.style.border = '2px solid red';
+      error.classList.add('error');
+      return error
+    }
     
     switch (type) {
 
       case 'text': 
         if (input.value == null || input.value == '') {
-          const error = document.createElement('p');
-          error.innerHTML = 'Veuillez saisir un nom';
-          input.insertAdjacentElement('afterend', error);
-        } else {
-          console.log('fail');
+          input.insertAdjacentElement('afterend', error('Veuillez saisir un nom'));
         }
       break;
 
       case 'email':
           if (input.value == null || input.value == '') {
-            const error = document.createElement('p');
-            error.innerHTML = 'Veuillez saisir une adresse mail valide';
-            input.insertAdjacentElement('afterend', error);
-          } else {
-            console.log('fail_email');
+            input.insertAdjacentElement('afterend', error('Veuillez saisir un email valide'));
           }
       break;
 
       case 'date':
           if (input.value == null || input.value == '') {
-            const error = document.createElement('p');
-            error.innerHTML = 'Veuillez saisir une date';
-            input.insertAdjacentElement('afterend', error);
-          } else {
-            console.log('fail_date');
+            input.insertAdjacentElement('afterend', error('Veuillez saisir une date'));
           }
       break;
 
       case 'radio':
-          console.log('test_radio' + inputs);
           for (const element of inputs) {
             if(!(element.type == 'radio' && element.checked)) {
               const radioLast = document.querySelector('.radioContainer');
               const error = document.createElement('p');
+              error.style.color = 'red';
+              error.style.fontSize = '15px';
               error.innerHTML = 'Veuillez saisir une ville';
               error.classList.add('errorRadio');
+              error.classList.add('error');
 
               if(!document.querySelector('.errorRadio')) {
                 radioLast.appendChild(error);
@@ -96,12 +103,16 @@ const handleForm = () => {
           if(!(document.querySelector('.checkboxRequired') && document.querySelector('.checkboxRequired').checked)) {
             const error = document.createElement('p');
             error.innerHTML = 'Veuillez selectionner le champ obligatoire';
+            error.style.color = 'red';
+            error.style.fontSize = '15px';
             error.classList.add('errorCheckbox');
-            lastCheckbox.appendChild(error);
+            error.classList.add('error');
+
+            if(!document.querySelector('.errorCheckbox')) {
+              lastCheckbox.appendChild(error);
+            }
           }
       break;
-
-
     }
   }
 }
